aef79f353101d0c8b81de90776d50b545141716e
LucasD1az <110778622+LucasD1az@users.noreply.github.com>
2023-06-20 17:06:09 -0300

    Ahora uso una funcion del mapa Comentado que hace cada funcion

--- Sokoban/src/library/UserSB.fs
+++ Sokoban/src/library/UserSB.fs
@@ -3,17 +3,8 @@ namespace SokobanUserDynamics
 open SokobanMapGenerator
 
 module user =
-    let getCoordinate (block : Block) =
-        match block with
-        | Wall (c, d) -> (c, d)
-        | Box (c, d) -> (c, d)
-        | BoxOnGoal (c, d) -> (c, d)
-        | Player (c, d) -> (c, d)
-        | PlayerOnGoal (c, d) -> (c, d)
-        | Goal (c, d) -> (c, d)
-        | Floor (c, d) -> (c, d)
-        | Outside (c, d) -> (c, d)
-
+    // isMovable decides if a block is movable given the next block
+    // e.g. if the next block is another box, it can't be moved
     let isMovable (oneBlockAway:Block) (twoBlocksAway:Block) :bool=
         match oneBlockAway with
         | Wall _ -> false
@@ -29,6 +20,8 @@ module user =
                             | _ -> true
         | _ -> true
 
+    // nextBlock returns the next block from coordinate coord, given the direction key and the map
+    // It must be noted that the x index goes from top to bottom, and the y index goes from left to right
     let nextBlock (map: Block list) (key: char) (coord:int*int) : Block =
         let nextCoord = match key with
                         | 'w' -> (fst coord-1, snd coord)
@@ -37,36 +30,38 @@ module user =
                         | 'd' -> (fst coord, snd coord+1)
                         | _ -> raise (InvalidChar ("Error en el movimiendo. El simbolo '" + string key + "' no tiene ningun significado. Los posibles simbolos son w, a, s, d."))
         map
-        |> List.find (fun x -> getCoordinate x = nextCoord)
+        |> List.find (fun x -> SBMap.castToTuple x = nextCoord)
 
+    // move moves the player and the corresponding blocks, so it
+    // returns a new map with the player and the blocks moved
     let move (oldmap:Block list) (key:char) :Block list =
         let player = oldmap
                             |>List.find(fun x -> match x with
                                                     | Player _ -> true
                                                     | PlayerOnGoal _ -> true
                                                     | _ -> false)
-        let playerCoord = getCoordinate player
+        let playerCoord = SBMap.castToTuple player
         let oneBlockAway = nextBlock oldmap key playerCoord
-        let twoBlocksAway = nextBlock oldmap key (getCoordinate oneBlockAway)
+        let twoBlocksAway = nextBlock oldmap key (SBMap.castToTuple oneBlockAway)
         if isMovable oneBlockAway twoBlocksAway then
             oldmap
             |> List.map (fun x -> match x with
-                                    | Player (c, d) -> Floor (c, d)
-                                    | PlayerOnGoal (c, d) -> Goal (c, d)
-                                    | _ when x = oneBlockAway -> match oneBlockAway with
+                                    | Player (c, d) -> Floor (c, d) // Player is moved to the next block, and the previous block is turned into a floor
+                                    | PlayerOnGoal (c, d) -> Goal (c, d) // Player is moved to the next block, and the previous block is turned into a goal
+                                    | _ when x = oneBlockAway -> match oneBlockAway with // The block one block away now has the player
                                                                     | Goal (c, d) -> PlayerOnGoal (c, d)
                                                                     | BoxOnGoal (c,d) -> PlayerOnGoal (c, d)
-                                                                    | _ -> Player (getCoordinate oneBlockAway)
-                                    | _ when x = twoBlocksAway -> match twoBlocksAway with
+                                                                    | _ -> Player (SBMap.castToTuple oneBlockAway)
+                                    | _ when x = twoBlocksAway -> match twoBlocksAway with // The block one block away now is the block two blocks away (because it is mmoved)
                                                                     | Goal (c,d) -> match oneBlockAway with
-                                                                                        | Box (e,f) -> BoxOnGoal (getCoordinate twoBlocksAway)
-                                                                                        | BoxOnGoal (e,f) -> BoxOnGoal (getCoordinate twoBlocksAway)
-                                                                                        | _ -> Goal (getCoordinate twoBlocksAway)
+                                                                                        | Box (e,f) -> BoxOnGoal (SBMap.castToTuple twoBlocksAway)
+                                                                                        | BoxOnGoal (e,f) -> BoxOnGoal (SBMap.castToTuple twoBlocksAway)
+                                                                                        | _ -> Goal (SBMap.castToTuple twoBlocksAway)
                                                                     | _ -> match oneBlockAway with
-                                                                                | Box (c,d) -> Box (getCoordinate twoBlocksAway)
-                                                                                | BoxOnGoal (c,d) -> Box (getCoordinate twoBlocksAway)
+                                                                                | Box (c,d) -> Box (SBMap.castToTuple twoBlocksAway)
+                                                                                | BoxOnGoal (c,d) -> Box (SBMap.castToTuple twoBlocksAway)
                                                                                 | _ -> twoBlocksAway
-                                    | _ -> x)
+                                    | _ -> x) // The rest of the blocks are left as they were
         else
-            oldmap
+            oldmap // If the block can't be moved, the map is left as it was
 
